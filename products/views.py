411cf7b9fd2174964588d1ebd2574aee635e1b00
Bonafs <bonafs@yahoo.com>
2025-07-11 13:51:29 +0100

    Test: Simple template to isolate 500 error

--- products/views.py
+++ products/views.py
@@ -8,7 +8,7 @@ from .forms import ProductSearchForm
 
 class ProductListView(ListView):
     model = Product
-    template_name = 'products/product_list_debug.html'  # Debug mode
+    template_name = 'products/product_list_simple.html'
     context_object_name = 'products'
     paginate_by = 12
     
