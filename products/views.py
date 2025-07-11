cdba934911a20474a08fa71f138c610c4189ea54
Bonafs <bonafs@yahoo.com>
2025-07-11 13:53:39 +0100

    Restore: Original template - 500 error caused by missing database migrations

--- products/views.py
+++ products/views.py
@@ -8,7 +8,7 @@ from .forms import ProductSearchForm
 
 class ProductListView(ListView):
     model = Product
-    template_name = 'products/product_list_simple.html'
+    template_name = 'products/product_list.html'
     context_object_name = 'products'
     paginate_by = 12
     
