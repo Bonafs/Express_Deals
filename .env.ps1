db954083a5aa710143c46fa6476157f365d14438
Bonafs <bonafs@yahoo.com>
2025-07-14 04:19:05 +0100

    Add .env.ps1

---
+++ .env.ps1
@@ -0,0 +1,3 @@
+# PowerShell profile for Express Deals local development
+$Env:CLOUDINARY_URL = "cloudinary://853483899852935:tFJ6Rb9xofDzV2Y1YrBZWaIZkhs@dzecfjfju"
+$Env:REDIS_URL = "rediss://:pa713568b0703561f67b730c73f37fd470aa444958d9a22a3f9ca0d8276e23d82@ec2-54-246-226-65.eu-west-1.compute.amazonaws.com:7480"
