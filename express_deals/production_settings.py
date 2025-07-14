001e6c77c91c0dd06a68a4a5cdb12352b10df15d
Bonafs <bonafs@yahoo.com>
2025-07-14 02:21:02 +0100

    Remove AWS S3 references; enforce Cloudinary as only media backend in production

--- express_deals/production_settings.py
+++ express_deals/production_settings.py
@@ -43,18 +43,9 @@ if os.getenv('SECURE_PROXY_SSL_HEADER'):
 # Static files settings for production
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# Media files settings for production (using cloud storage)
-if os.getenv('AWS_ACCESS_KEY_ID'):
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-    AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
-    AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME', 'us-east-1')
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-    AWS_DEFAULT_ACL = 'public-read'
-    AWS_S3_OBJECT_PARAMETERS = {
-        'CacheControl': 'max-age=86400',
-    }
+
+# Media files settings for production (Cloudinary enforced)
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Email settings for production
 if os.getenv('EMAIL_HOST'):
